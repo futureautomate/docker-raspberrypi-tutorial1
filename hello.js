943837340905f3d71b6d93ae93f761008199bc9e
futureautomate <futureautomate@gmail.com>
2023-04-02 12:53:18 +0530

    changes in js file for a pre final test in raspberry as wong urs in webhook

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Well Hello  worlds guys this is future automation!');
+  res.end('Again changed for the wrong url guys!');
 }).listen(8080); 
