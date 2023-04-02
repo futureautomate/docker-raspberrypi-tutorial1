3060e0c639bd5ca0ddb91f85adf6536de58b1943
futureautomate <futureautomate@gmail.com>
2023-04-02 12:51:24 +0530

    changes in js file for a pre final test in raspberry

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welll hello world guys!');
+  res.end('Well Hello  worlds guys this is future automation!');
 }).listen(8080); 
