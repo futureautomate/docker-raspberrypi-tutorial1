fd51ed7a4c248f979e29eda2bb6c7ff051be9778
futureautomate <futureautomate@gmail.com>
2023-04-07 00:47:34 +0530

    this is another trial of restopping and running

--- hello.js
+++ hello.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to the 10th anniversary meetup of docker again 10 times trial');
+  res.end('this is another trial of restopping and running');
 }).listen(8080); 
